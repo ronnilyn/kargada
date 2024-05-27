1fd63d6bee121e8d002494e5b86d8555ad879074
ronnilyn <ronilynarienza10@gmail.com>
2024-05-27 21:38:29 +0800

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+/***** Database Connection *****/
+class Connection{
+	private $servername = "localhost";
+	private $username = "root";
+	private $password = "";
+	private $dbname = "freightsystem";
+	private $dsn;
+	private $pdo;
+
+	public function conn(){
+		try {
+		$this->dsn = "mysql:host=".$this->servername.";dbname=".$this->dbname;
+		$this->pdo = new PDO($this->dsn, $this->username, $this->password);
+		$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+		return $this->pdo;			
+		} catch (Exception $e) {
+			echo 'error'. $e->getmessage();
+		}
+
+	}
+}
+
+
+?>
