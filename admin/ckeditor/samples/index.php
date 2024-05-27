442a03688bfa4fb450f9fcda0cc37045b02b3a9a
ronnilyn <ronilynarienza10@gmail.com>
2024-05-27 23:39:15 +0800

    Add files via upload

---
+++ admin/ckeditor/samples/index.php
@@ -0,0 +1,22 @@
+
+	<meta charset="utf-8">
+	<script src="../ckeditor.js"></script>
+	<script src="js/sample.js"></script>
+	<link rel="stylesheet" href="css/samples.css">
+	<link rel="stylesheet" href="toolbarconfigurator/lib/codemirror/neo.css">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="description" content="Try the latest sample of CKEditor 4 and learn more about customizing your WYSIWYG editor with endless possibilities.">
+
+
+
+
+				<div id="editor">
+					<h1>Hello world!</h1>
+					<p>I'm an instance of <a href="https://ckeditor.com">CKEditor</a>.</p>
+				</div>
+		
+
+<script>
+	initSample();
+</script>
+
